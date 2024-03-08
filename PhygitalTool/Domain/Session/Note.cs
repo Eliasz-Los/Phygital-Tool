a78c299ef8494d203cd8fe72b6454de2f3c648cc
Jonas Wuyten <Jonas.wuyten@student.kdg.be>
2024-03-08 14:39:17 +0100

    session & subplatform

---
+++ PhygitalTool/Domain/Session/Note.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Session;
+
+public class Note
+{
+    public int installationID { get; set; }
+    public string comment { get; set; }
+}
---
+++ PhygitalTool/Domain/Session/Participation.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Session;
+
+public class Participation
+{
+    public int participationID { get; set; }
+    public TimeOnly participationDuration { get; set; }
+    public int amountOfParticipations { get; set; }
+
+}
