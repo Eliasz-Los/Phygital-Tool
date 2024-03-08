9bf2836bb8d9590c8e62832b77943610496e6b40
Jonas Wuyten <Jonas.wuyten@student.kdg.be>
2024-03-08 15:18:48 +0100

    De links tussen de klassen van Session & Version packages gelegd

--- PhygitalTool/Domain/Session/Installation.cs
+++ PhygitalTool/Domain/Session/Installation.cs
@@ -4,4 +4,6 @@ public class Installation
 {
     public int installationID { get; set; }
     public string location { get; set; }
+    public ICollection<Participation> Participations { get; set; }
+
 }
--- PhygitalTool/Domain/Session/Note.cs
+++ PhygitalTool/Domain/Session/Note.cs
@@ -4,4 +4,5 @@ public class Note
 {
     public int installationID { get; set; }
     public string comment { get; set; }
+    public Participation Participation { get; set; }
 }
