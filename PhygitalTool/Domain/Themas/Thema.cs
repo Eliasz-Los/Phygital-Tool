cdc7f447ed5831a1866798c3d8e22ab14595e7bc
Arthur Linsen <arthur.linsen@student.kdg.be>
2024-03-08 14:28:53 +0100

    Domain - Account package

---
+++ PhygitalTool/Domain/Themas/Thema.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Thema;
+
+public class Thema
+{
+    public long Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public ICollection<Thema> SubThemas { get; set; }
+
+    public Thema(string title, string description, ICollection<Thema> subThemas)
+    {
+        Title = title;
+        Description = description;
+        SubThemas = subThemas;
+    }
+}
--- PhygitalTool/UI_MVC/Program.cs
+++ PhygitalTool/UI_MVC/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// AddRoles()
+// builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//     .AddRoles<IdentityRole>()
+//     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
