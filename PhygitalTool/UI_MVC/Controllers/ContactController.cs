68c5ccfc361b7fc3d176120c06a103d57a552958
Arthur Linsen <arthur.linsen@student.kdg.be>
2024-03-15 21:40:01 +0100

    US 90 asp TODO US 91 code behind TODO

---
+++ PhygitalTool/UI_MVC/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Phygital.UI_MVC.Controllers;
+
+public class ContactController : Controller
+{
+    private readonly ILogger<QuestionController> _logger;
+    
+    public ContactController(ILogger<QuestionController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
