da9d8111c0676a1f46f1b8c65678fbe4551cbcc0
Arthur Linsen <arthur.linsen@student.kdg.be>
2024-03-15 15:32:38 +0100

    US 88, completed

---
+++ PhygitalTool/UI_MVC/Controllers/EndpageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Phygital.UI_MVC.Controllers;
+
+public class EndpageController : Controller
+{
+    private readonly ILogger<QuestionController> _logger;
+    
+    public EndpageController(ILogger<QuestionController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
