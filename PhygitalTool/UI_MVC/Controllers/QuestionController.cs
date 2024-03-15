651437a369b04bdb9d53bb2514408a538315074e
Arthur Linsen <arthur.linsen@student.kdg.be>
2024-03-15 15:01:47 +0100

    riderect naar question page

---
+++ PhygitalTool/UI_MVC/Controllers/QuestionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Phygital.UI_MVC.Controllers;
+
+public class QuestionController : Controller
+{
+    private readonly ILogger<QuestionController> _logger;
+    
+    public QuestionController(ILogger<QuestionController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
