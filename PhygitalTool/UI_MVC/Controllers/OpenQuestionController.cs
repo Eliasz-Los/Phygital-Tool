7d8f304dd9913c2e1eb0b9594bd8c13b827e975b
Josse Dresselaers <josse.dresselaers@student.kdg.be>
2024-03-18 11:11:10 +0100

    We hebben een titel en tekstveld en zelfs een controller (half)!

---
+++ PhygitalTool/UI_MVC/Controllers/OpenQuestionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phygital.BL;
+
+namespace Phygital.UI_MVC.Controllers;
+
+public class OpenQuestionController: Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    /*
+    private readonly IOpenQuestionManager _flowManager;
+    */
+    private readonly UnitOfWork _uow;
+
+    // TODO manager moet hier nog in
+    public OpenQuestionController(ILogger<HomeController> logger, UnitOfWork uow)
+    {
+        _logger = logger;
+        _uow = uow;
+    }
+
+    public IActionResult Index()
+    {
+        //var OpenQuestion = _flowManager.GetOpenQuestion();
+        // TODO question meegeven zodat deze gedisplayed kan worden
+        return View();
+    }
+
+}
