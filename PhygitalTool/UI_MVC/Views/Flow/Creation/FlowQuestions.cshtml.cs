e1a5640e8a8a2267c442032b04f71960c30c30ab
Josse Dresselaers <josse.dresselaers@student.kdg.be>
2024-04-23 09:24:27 +0200

    Paginas voor flowcreation

---
+++ PhygitalTool/UI_MVC/Views/Flow/Creation/FlowQuestions.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Phygital.UI_MVC.Views.Flow.Creation;
+
+public class FlowQuestions : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ PhygitalTool/UI_MVC/Views/Flow/Creation/FlowThemeAndType.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Phygital.UI_MVC.Views.Flow.Creation;
+
+public class FlowThemeAndType : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
