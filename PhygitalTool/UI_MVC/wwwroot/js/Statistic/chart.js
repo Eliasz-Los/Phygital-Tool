cfb6c84aa49f962e565f65c77c68dd8f741c245a
Eliasz Los <eliasz.los@student.kdg.be>
2024-04-26 10:46:23 +0200

    statistics Ui changes

--- PhygitalTool/UI_MVC/wwwroot/js/Statistic/chart.js
+++ PhygitalTool/UI_MVC/wwwroot/js/Statistic/chart.js
@@ -40,11 +40,52 @@ function createChart(tableId, chartId, questionText) {
     return chart;
 }
 
+function createDoughnutChart(tableId, chartId, questionText) 
+{
+    var table = document.getElementById(tableId);
+    if (!table) {
+        console.log('Table not found: ' + tableId);
+        return;
+    }
+    var rows = table.querySelectorAll('tbody tr');
+
+    var labels = [];
+    var data = [];
+
+    rows.forEach(function(row) {
+        var cells = row.querySelectorAll('td');
+        labels.push(cells[0].textContent);
+        data.push(parseInt(cells[1].textContent));
+    });
+
+    var ctx = document.getElementById(chartId).getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'doughnut', // Change this line
+        data: {
+            labels: labels,
+            datasets: [{
+                label: questionText,
+                data: data,
+                backgroundColor: [
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)',
+                    'rgb(75,192,91)',
+                ],
+                //backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        }
+    });
+    return chart;
+}
+
 $(document).ready(function() {
     var tables = document.querySelectorAll('table[id^="table-"]');
     tables.forEach(function(table) {
         var safeId = table.id.replace('table-', '');
-        createChart('table-' + safeId, 'chart-' + safeId, safeId);
+        createDoughnutChart('table-' + safeId, 'chart-' + safeId, safeId);
     });
 });
 
