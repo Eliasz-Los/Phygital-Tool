20d0762a176052d2ffc18068d32fb71b8c119997
Jonas Wuyten <Jonas.wuyten@student.kdg.be>
2024-04-26 11:16:40 +0200

    Thema aanpas pagina gemaakt, nog laten werken

--- PhygitalTool/DAL/EF/ThemeRepository.cs
+++ PhygitalTool/DAL/EF/ThemeRepository.cs
@@ -16,6 +16,11 @@ public class ThemeRepository : IThemeRepository
         _dbContext.Themas.Add(theme);
     }
 
+    public Theme ReadThemeById(long id)
+    {
+        return _dbContext.Themas.Find(id);
+    }
+
     public IEnumerable<Theme> ReadAllThemas()
     {
         return _dbContext.Themas.Select(t => t);
@@ -29,6 +34,16 @@ public class ThemeRepository : IThemeRepository
     public IEnumerable<Theme> ReadSubThemasFlow(long flowId)
     {
         return _dbContext.Themas.Where(t => t.Flows.Any(f => f.Id == flowId)).Select(t => t);
+    }
+
+    void IThemeRepository.DeleteThemeById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateTheme(Theme theme)
+    {
+        throw new NotImplementedException();
     }
 
     public IEnumerable<Theme> DeleteThemeById(long id)
--- PhygitalTool/UI_MVC/Controllers/ThemaController.cs
+++ PhygitalTool/UI_MVC/Controllers/ThemaController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Phygital.BL;
+using Phygital.UI_MVC.Models.Dto;
 
 namespace Phygital.UI_MVC.Controllers;
 
@@ -6,10 +8,13 @@ public class ThemaController : Controller
 
 {
     private readonly ILogger<ThemaController> _logger;
+    private readonly IThemeManager _themeManager;
 
-    public ThemaController(ILogger<ThemaController> logger)
+
+    public ThemaController(ILogger<ThemaController> logger, IThemeManager themeManager)
     {
         _logger = logger;
+        _themeManager = themeManager;
     }
 
     public IActionResult Index()
@@ -19,5 +24,27 @@ public class ThemaController : Controller
     public IActionResult Add()
     {
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult Edit(long id)
+    {
+        var thema = _themeManager.GetThemeById(id);
+        return View(thema);
+    }
+
+
+    [HttpPost]
+    public IActionResult Edit(long id, SubThemasDto thema)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        else
+        {
+            _themeManager.ChangeTheme(thema.Id, thema.Title, thema.Description);
+            return RedirectToAction("Index", "Thema", new {id = thema.Id});
+        }
     }
 }
