601bf0862ae15497c3f8b81e57dbff50ee3b381d
Arthur Linsen <arthur.linsen@student.kdg.be>
2024-04-30 14:13:21 +0200

    fixes from merge

--- PhygitalTool/UI_MVC/Controllers/FlowController.cs
+++ PhygitalTool/UI_MVC/Controllers/FlowController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Phygital.BL;
 using Phygital.Domain;
+using Phygital.UI_MVC.Models.Dto;
 
 namespace Phygital.UI_MVC.Controllers;
 
@@ -9,15 +10,17 @@ public class FlowController : Controller
 {
     private readonly ILogger<FlowController> _logger;
     private readonly IFlowManager _flowManager;
+    private readonly IThemeManager _themeManager;
     private readonly UnitOfWork _uow;
-    
-    public FlowController(ILogger<FlowController> logger, IFlowManager flowManager, UnitOfWork uow)
+
+    public FlowController(ILogger<FlowController> logger, IFlowManager flowManager, IThemeManager themeManager,
+        UnitOfWork uow)
     {
         _logger = logger;
         _flowManager = flowManager;
+        _themeManager = themeManager;
         _uow = uow;
     }
-    
 
     public IActionResult Index()
     {
@@ -34,5 +37,56 @@ public class FlowController : Controller
     public IActionResult Add()
     {
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult Edit(long id)
+    {
+        var flow = _flowManager.GetFlowById(id);
+        var themes = _themeManager.GetAllThemas();
+        ViewBag.Themes = themes;
+        return View(flow);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(long id, FlowDto flow)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _uow.BeginTransaction();
+            _flowManager.ChangeFlow(flow.Id, flow.FlowType, flow.IsOpen, flow.ThemeId);
+            _uow.Commit();
+            return RedirectToAction("Index", new { id = flow.Id });
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error updating flow with id {Id}", id);
+            ModelState.AddModelError("", "An error occurred while updating the flow.");
+            return View();
+        }
+    }
+
+    [HttpPost]
+    public IActionResult Delete(long id)
+    {
+        _uow.BeginTransaction();
+        _flowManager.RemoveFlow(id);
+        _uow.Commit();
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult FlowThemeAndType()
+    {
+        return View("Creation/FlowThemeAndType");
+    }
+    
+    public IActionResult FlowQuestions()
+    {
+        return View("Creation/FlowQuestions");
     }
 }
