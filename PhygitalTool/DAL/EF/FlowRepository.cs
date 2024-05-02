2ee775ea3335ba2d5d7e299c5aecfe1734d379ac
Eliasz Los <eliasz.los@student.kdg.be>
2024-05-02 22:46:56 +0200

    bug fixes v1

--- PhygitalTool/DAL/EF/FlowRepository.cs
+++ PhygitalTool/DAL/EF/FlowRepository.cs
@@ -35,6 +35,20 @@ public class FlowRepository : IFlowRepository
     public void DeleteFlow(long id)
     {
         var flowToDelete = _dbContext.Flows.Find(id);
+        
+        // Omdat we de Flow willen verwijderen, moeten we eerst alle FlowElements die aan deze Flow gerelateerd zijn hun link verbreken oftwel CascadeDelete().
+        var relatedFlowElements = _dbContext.FlowElements.Where(fe => fe.Flow.Id == id);
+        foreach (var flowElement in relatedFlowElements)
+        {
+            flowElement.Flow = null; 
+        }
+        
+        var relatedAnswers = _dbContext.Answers.Where(a => a.Flow.Id == id);
+        foreach (var answer in relatedAnswers)
+        {
+            answer.Flow = null;
+        }
+        
         _dbContext.Flows.Remove(flowToDelete);
     }
 }
