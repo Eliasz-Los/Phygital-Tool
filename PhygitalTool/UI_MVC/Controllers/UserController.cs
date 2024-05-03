2f3978715fc590b0cd263a32d0a401cdb315540f
Jonas Wuyten <Jonas.wuyten@student.kdg.be>
2024-05-03 10:43:45 +0200

    User Registration Page

---
+++ PhygitalTool/UI_MVC/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Phygital.UI_MVC.Controllers;
+
+public class UserController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
