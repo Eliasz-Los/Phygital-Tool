9801bd5b37ea4f20ae814fdd80794e1ec94e8554
Josse Dresselaers <josse.dresselaers@student.kdg.be>
2024-05-13 11:29:45 +0200

    OpenQuestionspagina gemaakt

---
+++ PhygitalTool/UI_MVC/Controllers/OpenquestionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phygital.BL;
+
+namespace Phygital.UI_MVC.Controllers;
+
+public class OpenquestionController : Controller
+{
+    private readonly ILogger<OpenquestionController> _logger;
+    private readonly IFlowElementManager _FlowElementManager;
+    private readonly UnitOfWork _uow;
+    
+    public OpenquestionController(ILogger<OpenquestionController> logger, IFlowElementManager flowElementManager, UnitOfWork uow)
+    {
+        _logger = logger;
+        _FlowElementManager = flowElementManager;
+        _uow = uow;
+    }
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult AddOpenQuestion()
+    {
+        return View("Creation/AddOpenQuestion");
+    }
+}
