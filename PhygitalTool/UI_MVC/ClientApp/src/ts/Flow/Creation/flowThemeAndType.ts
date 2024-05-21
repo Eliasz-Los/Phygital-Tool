6c96b9580c0baa72160adc759a5bf655ca771c23
Josse Dresselaers <josse.dresselaers@student.kdg.be>
2024-05-21 15:04:46 +0200

    Typescript wekt voor flows

--- PhygitalTool/UI_MVC/ClientApp/src/ts/Flow/Creation/flowThemeAndType.ts
+++ PhygitalTool/UI_MVC/ClientApp/src/ts/Flow/Creation/flowThemeAndType.ts
@@ -1,4 +1,4 @@
-import {addFlowData} from "./flowThemeAndTypeRest";
+import {addFlow} from "./flowThemeAndTypeRest";
 import {fillSubthemesSelect} from "./flowThemeAndTypeRest";
 
 export interface SubTheme {
@@ -17,19 +17,19 @@ const addButton: HTMLElement | null = document.getElementById("submitFlow");
 const addQuestion: HTMLElement | null = document.getElementById("getquestion");
 
 
-export async function addFlow(): Promise<void> {
+export async function addFlowData(): Promise<void> {
     const selectedType: HTMLInputElement = document.getElementById('TypeSelect') as HTMLInputElement;
-    const selectedTheme: HTMLSelectElement = document.getElementById('themeSelect') as HTMLSelectElement;
+    const selectedTheme: HTMLSelectElement = document.getElementById('ThemaSelect') as HTMLSelectElement;
     const selectedThemeId = selectedTheme.options[selectedTheme.selectedIndex].value;
     const isActive: HTMLInputElement = document.getElementById('ActiveCheckbox') as HTMLInputElement;
 
-    var data: flowObject = {
+    const data: flowObject = {
         FlowType: selectedType.value,
         IsOpen: isActive.checked,
         ThemeId: parseInt(selectedThemeId)
     };
     
-    await addFlowData(data)
+    await addFlow(data)
         .then(response => {
             console.log(response);
         })
@@ -39,17 +39,36 @@ export async function addFlow(): Promise<void> {
 }
 
 
-fillSubthemesSelect()
-    .then(response => {
-        console.log(response);
-    })
-    .catch(error => {
-        console.error(error);
-    });
+async function populateSubthemesSelect() {
+    try {
+        const subThemas: SubTheme[] = await fillSubthemesSelect();
+
+        const output = document.getElementById("ThemaSelect") as HTMLSelectElement;
+        if (!output) {
+            throw new Error("ThemaSelect element not found");
+        }
+
+        let bodyData = ``;
+        for (const subThema of subThemas) {
+            bodyData += `
+                <option value="${subThema.id}" data-description="${subThema.description}">${subThema.title}</option>
+            `;
+        }
+        output.innerHTML += bodyData;
+
+    } catch (error) {
+        console.error('Error:', error);
+        alert("Something went wrong while fetching subthemes. Check the console for more details.");
+    }
+}
+
+populateSubthemesSelect();
+
 
 if (addButton) {
-    addButton.addEventListener("click", addFlow);
+    addButton.addEventListener("click", addFlowData);
 }
+
 if (addQuestion) {
-    addQuestion.addEventListener("click", addFlow);
+    addQuestion.addEventListener("click", addFlowData);
 }
