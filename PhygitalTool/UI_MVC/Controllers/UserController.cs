7ccf0db70e1b5447071e424f3e6742680ee2a035
Arthur Linsen <arthur.linsen@student.kdg.be>
2024-05-24 15:50:37 +0200

    rubbish

--- PhygitalTool/UI_MVC/Controllers/UserController.cs
+++ PhygitalTool/UI_MVC/Controllers/UserController.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Phygital.BL;
+using Phygital.BL.Managers;
+using Phygital.Domain.User;
+using Phygital.UI_MVC.Models;
 
 namespace Phygital.UI_MVC.Controllers;
 
 public class UserController : Controller
 {
     private readonly ILogger<ThemaController> _logger;
-    private readonly IUserManager _userManager;
+    private readonly UserManager<Account> _userManager;
     private readonly UnitOfWork _uow;
 
-    public UserController(ILogger<ThemaController> logger, IUserManager userManager, UnitOfWork uow)
+    public UserController(ILogger<ThemaController> logger, UserManager<Account> userManager, UnitOfWork uow)
     {
         _logger = logger;
         _userManager = userManager;
@@ -19,13 +23,20 @@ public class UserController : Controller
     
     [HttpGet]
     [Authorize(Roles = "Admin")]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
         var loggedInUserEmail = User.Identity.Name;
-        
-        var loggedInUser = _userManager.GetUserByEmail(loggedInUserEmail);
+
+        var loggedInUser = await _userManager.FindByNameAsync(User.Identity.Name);
+        if (loggedInUser?.Organisation == null)
+        {
+            // Handle case when user or user's organisation is not found
+            _logger.LogError("User with email {Email} not found or has no organisation", loggedInUserEmail);
+        }
+
         var users = _userManager.GetUsersByOrganisationId(loggedInUser.Organisation.id);
-        return View(users);
+        var user = loggedInUser.Organisation.id;
+        return View(user);
     }
     
     [HttpPost]
