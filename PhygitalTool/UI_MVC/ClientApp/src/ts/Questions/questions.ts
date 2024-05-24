5e439b28e21faf5f4ff1536a14edead4ef68a4ab
Josse Dresselaers <josse.dresselaers@student.kdg.be>
2024-05-24 13:23:40 +0200

    Question toevoegen werkt in typescript

--- PhygitalTool/UI_MVC/ClientApp/src/ts/Questions/questions.ts
+++ PhygitalTool/UI_MVC/ClientApp/src/ts/Questions/questions.ts
@@ -1,6 +1,7 @@
 import {fillSubthemesSelect} from "./questionsRest";
 import {sendOptions} from "./questionsRest";
 import {addQuestion} from "./questionsRest";
+import {SubTheme} from "../Flow/Creation/flowThemeAndType";
 
 export interface Question {
     Text: string;
@@ -62,25 +63,55 @@ export async function collectOptions(): Promise<void> {
     await sendOptions(options);
 }
 
+async function populateSubthemesSelect() {
+    try {
+        const subThemas: SubTheme[] = await fillSubthemesSelect();
+
+        const output = document.getElementById("ThemaSelect") as HTMLSelectElement;
+        if (!output) {
+            throw new Error("ThemaSelect element not found");
+        }
+
+        let bodyData = ``;
+        for (const subThema of subThemas) {
+            bodyData += `
+                <option value="${subThema.id}" data-description="${subThema.description}">${subThema.title}</option>
+            `;
+        }
+        output.innerHTML += bodyData;
+
+    } catch (error) {
+        console.error('Error:', error);
+        alert("Something went wrong while fetching subthemes. Check the console for more details.");
+    }
+}
+
 
 export async function addQuestionData(): Promise<void> {
     await collectOptions();
-
     const questionTitle = (document.getElementById('QuestionTitle') as HTMLInputElement).value;
     const selectedTheme = document.getElementById('ThemaSelect') as HTMLSelectElement;
     const selectedThemeId = selectedTheme.options[selectedTheme.selectedIndex].value;
     const isActive = (document.getElementById('ActiveCheckbox') as HTMLInputElement).checked;
     const selectedType = (document.getElementById('TypeSelect') as HTMLSelectElement).value;
 
-    const data = {
+    const data: Question = {
         Text: questionTitle,
         isActive: isActive,
-        SubTheme: parseInt(selectedThemeId, 10),
+        SubTheme: parseInt(selectedThemeId),
         Type: selectedType
     };
-    
-    await addQuestion(data);
+
+    await addQuestion(data)
+        .then(response => {
+        alert(response);
+    })
+        .catch(error => {
+            alert(error);
+        });
 }
+
+populateSubthemesSelect();
 
 if (optionButton) {
     optionButton.addEventListener("click", addOption);
@@ -89,5 +120,5 @@ if (optionButton) {
 if (submitQuestion) {
     submitQuestion.addEventListener("click", addQuestionData);
 }
-fillSubthemesSelect();
+
 
