03971b466d9dac60eb9f559b5e8330b682297ab2
Josse Dresselaers <josse.dresselaers@student.kdg.be>
2024-05-25 15:54:06 +0200

    imagedto aangepast

--- PhygitalTool/Domain/Questionsprocess/Infos/Image.cs
+++ PhygitalTool/Domain/Questionsprocess/Infos/Image.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
 using Phygital.Domain.Themas;
 
 namespace Phygital.Domain.Questionsprocess.Infos;
@@ -12,7 +14,12 @@ public class Image : Info
     [MinLength(3, ErrorMessage = "Title is too short, min 3 characters.")]
     public string Title { get; set; }
     [Required(ErrorMessage = "Url is required.")]
+    [DataType(DataType.ImageUrl)]
     public string Url { get; set; }
+    
+    [NotMapped]
+    public virtual IFormFile ImageFile { get; set; }
+    
     [Required(ErrorMessage = "AltText is required.")]
     [MaxLength(1000, ErrorMessage = "AltText is too long, max 1000 characters.")]
     public string AltText { get; set; }
