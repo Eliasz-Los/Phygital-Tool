ff0f6cfc38564d2d6ee0df303eb9dc417c3da9d4
Eliasz Los <eliasz.los@student.kdg.be>
2024-06-07 23:50:19 +0200

    cleaning comments

--- PhygitalTool/Domain/Questionsprocess/Questions/SingleChoiceQuestion.cs
+++ PhygitalTool/Domain/Questionsprocess/Questions/SingleChoiceQuestion.cs
@@ -4,7 +4,7 @@ namespace Phygital.Domain.Questionsprocess.Questions;
 
 public class SingleChoiceQuestion : Question
 {
-    [MaxLength(500, ErrorMessage = "Text is too long, max 500 characters.")]
+    [MaxLength(1000, ErrorMessage = "Text is too long, max 1000 characters.")]
     public override string Text { get; set; }
     public override bool Active { get; set; }
     public override int SequenceNumber { get; set; }
