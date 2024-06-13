dc5aaddd1bec96dc28e08d6c11df9aad5a8d401c
Eliasz Los <eliasz.los@student.kdg.be>
2024-06-13 21:21:31 +0200

    send grid api env bug

--- PhygitalTool/UI_MVC/Services/EmailSender.cs
+++ PhygitalTool/UI_MVC/Services/EmailSender.cs
@@ -7,9 +7,15 @@ namespace Phygital.UI_MVC.Services;
 
 public class EmailSender : IEmailSender
 {
+    private readonly IConfiguration _configuration;
+    
+    public EmailSender(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        var apiKey = Environment.GetEnvironmentVariable("send_grid_api");
+        var apiKey = _configuration.GetConnectionString("send_grid_api"); 
         var client = new SendGridClient(apiKey);
         var msg = new SendGridMessage()
         {
